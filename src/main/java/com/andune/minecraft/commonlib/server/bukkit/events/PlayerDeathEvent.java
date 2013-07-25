225f11b9272d20eee704dc3a1613e3ec808c58bf
Andune <andune.alleria@gmail.com>
2013-07-24 22:18:13 -0700

    add missing PlayerDeathEvent

---
+++ src/main/java/com/andune/minecraft/commonlib/server/bukkit/events/PlayerDeathEvent.java
@@ -0,0 +1,11 @@
+package main.java.com.andune.minecraft.commonlib.server.bukkit.events;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: morganm
+ * Date: 7/24/13
+ * Time: 9:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PlayerDeathEvent {
+}
